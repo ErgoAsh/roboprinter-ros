7e4650a0d1569a8e3087613e4bc81c053a1c011e
Marek K <ErgoAsh@users.noreply.github.com>
2022-12-07 11:27:44 +0100

    Fix common mistakes, let it compile and run

--- roboprinter/src/move_robot.cpp
+++ roboprinter/src/move_robot.cpp
@@ -12,18 +12,18 @@
 
 const double x_offset = 0;
 const double y_offset = 0;
-const double x_scale = -1;
-const double y_scale = 1;
+const double x_scale = -0.01;
+const double y_scale = 0.01;
 
-const double z_drawing = 1;
-const double z_movement = 10;
+const double z_drawing = 0.01;
+const double z_movement = 0.1;
 
-geometry_msgs::msg:Pose new_pose(double& x, double& y, bool is_drawing) const {
+geometry_msgs::msg::Pose new_pose(double& x, double& y, bool is_drawing) {
     geometry_msgs::msg::Pose pose;
-    msg.orientation.w = 1;
-    msg.position.x = x_scale * x + x_offset;
-    msg.position.y = y_scale * y + y_offset;
-    msg.position.z = is_drawing ? z_drawing, z_movement;
+    pose.orientation.w = 1;
+    pose.position.x = x_scale * x + x_offset;
+    pose.position.y = y_scale * y + y_offset;
+    pose.position.z = is_drawing ? z_drawing : z_movement;
 
     return pose;
 }
@@ -37,7 +37,7 @@ int main(int argc, char * argv[])
     );
 
     // Create a ROS logger
-    auto const logger = rclcpp::get_logger("roboprinter-logger");
+    auto const logger = rclcpp::get_logger("roboprinter");
     RCLCPP_INFO(logger, "Startup");
 
     // Load .svg file
@@ -45,28 +45,30 @@ int main(int argc, char * argv[])
     doc.LoadFile("/home/ergoash/roboprinter_ws/src/drawing.svg");
     RCLCPP_INFO(logger, "TinyXML output after loading file: %s", doc.ErrorStr()); 
 
-    std::vector<geometry::msg::Pose> poses;
-    tinyxml2::XMLElement current_element = doc.FirstChildElement("svg")->FirstChildElement("g").FirstChildElement();
+    std::vector<geometry_msgs::msg::Pose> poses;
+    tinyxml2::XMLElement* current_element = doc.FirstChildElement("svg")->FirstChildElement("g")->FirstChildElement();
     do {
         // TODO add support for other elements than "path"
         // TODO add support for splitting values using " " alongside ","
 
         // get "d" attribuute
         const char* path_cstr = "null";
-        current_element->querystringattribute("d", &path_cstr);
+        current_element->QueryStringAttribute("d", &path_cstr);
 
         // split "d" attribute content into substrings by spaces
         const std::string new_str = path_cstr;
-        std::vector<std::string> path = rcpputils::split(new_path, " ", skip_empty=true);
+        std::vector<std::string> path = rcpputils::split(new_str, ' ', true); // skip_empty = true
 
         // prepare poses depending on a given task
         bool is_in_absolute_state = false;
-        for (std::size_t i = 0; i < output.size(); i++) {
+        for (std::size_t i = 0; i < path.size(); i++) {
             double x = 0; // cached x value
             double y = 0; // cached y value
+            RCLCPP_INFO(logger, "XML attr: %s", path[i].c_str());
     
-            if (path[i] == "m") {
-                auto substring = rcpputils::split(path[i+i], ",", skip_empty=true);
+            if (path[i] == "M") {
+                auto substring = rcpputils::split(path[i+1], ',', true);
+
                 x = std::stod(substring[0]);
                 y = std::stod(substring[1]);
                 is_in_absolute_state = true;
@@ -79,7 +81,8 @@ int main(int argc, char * argv[])
             }
     
             if (path[i] == "m") {
-                auto substring = rcpputils::split(path[i+i], ",", skip_empty=true);
+                auto substring = rcpputils::split(path[i+1], ',', true);
+
                 x += std::stod(substring[0]);
                 y += std::stod(substring[1]);
                 is_in_absolute_state = false;
@@ -91,10 +94,10 @@ int main(int argc, char * argv[])
                 continue;
             }
     
-            if (path[i] == "h") {
+            if (path[i] == "H") {
                 x = std::stod(path[i+1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
@@ -102,15 +105,15 @@ int main(int argc, char * argv[])
             if (path[i] == "h") {
                 x += std::stod(path[i+1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
     
-            if (path[i] == "v") {
+            if (path[i] == "V") {
                 y = std::stod(path[i+1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
@@ -118,34 +121,34 @@ int main(int argc, char * argv[])
             if (path[i] == "v") {
                 y += std::stod(path[i+1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
     
-            if (path[i] == "l") {
-                auto substring = rcpputils::split(path[i+i], ",", skip_empty=true);
+            if (path[i] == "L") {
+                auto substring = rcpputils::split(path[i+1], ',', true);
                 x = std::stod(substring[0]);
                 y = std::stod(substring[1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
     
             if (path[i] == "l") {
-                auto substring = rcpputils::split(path[i+i], ",", skip_empty=true);
+                auto substring = rcpputils::split(path[i+1], ',', true);
                 x += std::stod(substring[0]);
                 y += std::stod(substring[1]);
     
-                poses.push_back(x, y, true);
+                poses.push_back(new_pose(x, y, true));
                 i++; // skip one iteration
                 continue;
             }
     
             // Equals to "L" or "l" command
             if (isdigit(path[i][0])) {
-                auto substring = rcpputils::split(path[i+i], ",", skip_empty=true);
+                auto substring = rcpputils::split(path[i], ',', true);
                 // Type depends on previous "M"/"m" command
                 if (is_in_absolute_state) {
                     x = std::stod(substring[0]);
@@ -156,7 +159,7 @@ int main(int argc, char * argv[])
                 }
     
                 poses.push_back(new_pose(x, y, true));
-                // Do not skip iteration
+                // Do not skip one iteration
                 continue;
             }
     
@@ -164,39 +167,33 @@ int main(int argc, char * argv[])
         }
 
         // Find new sibling for next iteration
-        current_element = current_element.nextsiblingelement();
+        current_element = current_element->NextSiblingElement();
     } while (current_element != nullptr);
+    RCLCPP_INFO(logger, "Created %d poses", (int) poses.size());
 
     // Create the MoveIt MoveGroup Interface
     using moveit::planning_interface::MoveGroupInterface;
     auto move_group_interface = MoveGroupInterface(node, "panda_arm"); // TODO roboprinter
 
-    // Set a target Pose
-    auto const target_pose = []{
-        geometry_msgs::msg::Pose msg;
-        msg.orientation.w = 1.0;
-        msg.position.x = 0.28;
-        msg.position.y = -0.2;
-        msg.position.z = 0.5;
-        return msg;
-    }();
-    move_group_interface.setPoseTarget(target_pose);
+    RCLCPP_INFO(logger, "Begin setting targets...");
+    move_group_interface.setPoseTargets(poses);
 
     // Create a plan to that target pose
-    /*
-       auto const [success, plan] = [&move_group_interface]{
-       moveit::planning_interface::MoveGroupInterface::Plan msg;
-       auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-       return std::make_pair(ok, msg);
-       }();
+    RCLCPP_INFO(logger, "Begin planning...");
+    auto const [success, plan] = [&move_group_interface] {
+        moveit::planning_interface::MoveGroupInterface::Plan msg;
+        auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+        return std::make_pair(ok, msg);
+    }();
 
     // Execute the plan
     if(success) {
-    move_group_interface.execute(plan);
+        RCLCPP_INFO(logger, "Planning succeeded!");
+        move_group_interface.execute(plan);
+        RCLCPP_INFO(logger, "Plan executed!");
     } else {
-    RCLCPP_ERROR(logger, "Planning failed!");
+        RCLCPP_ERROR(logger, "Planning failed!");
     }
-    */
 
     // Shutdown ROS
     rclcpp::shutdown();
--- run_docker.sh
+++ run_docker.sh
@@ -5,10 +5,14 @@ export WORK_DIR=/home/ergoash/roboprinter_ws
 
 docker run -it --rm \
     --network host \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    --device=/dev/dri \
+    --group-add video \
+    --env DISPLAY=$DISPLAY \
+    --volume /tmp/.X11-unix/:/tmp/.X11-unix \
+    --env=QT_X11_NO_MITSHM=1 \
     -v $(pwd):$WORK_DIR/src \
     -w $WORK_DIR/ \
     ergoash/roboprinter-ros \
     bash
     #/bin/bash -ci "source /root/.bashrc && /bin/bash"
+    #-m=2G \
